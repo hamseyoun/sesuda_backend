cb2e6be15ea5c86e6b7dd0d98028642fa44570a8
osumin <osn9274@naver.com>
2023-03-27 22:58:46 +0900

    멤버인원테스트 추가

---
+++ src/main/java/sesuda/dto/MemberDTO.java
@@ -0,0 +1,11 @@
+package sesuda.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+    private String userEmail;
+    private String userPassword;
+    private String userDisplayName;
+
+}
---
+++ src/main/java/sesuda/service/MemberService.java
@@ -0,0 +1,14 @@
+package sesuda.service;
+
+import sesuda.dto.MemberDTO;
+
+
+import java.util.List;
+
+public interface MemberService {
+
+    // 멤버인원
+    public List<MemberDTO> memberList();
+
+  
+}
