00e604f45c95fa3067e34114886bc54bc55dd093
seyoun <seyounham@gmail.com>
2023-03-27 13:44:27 +0900

    first commit

---
+++ src/main/java/sesuda/SesudaApplication.java
@@ -0,0 +1,13 @@
+package sesuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesudaApplication.class, args);
+	}
+
+}
