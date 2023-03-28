4b64b4881b2fd468ebc947a4ac48534ddff6d3fa
seyoun <seyounham@gmail.com>
2023-03-28 22:36:57 +0900

    test1 컨트롤러 추가

--- src/main/java/sesuda/controller/TestController.java
+++ src/main/java/sesuda/controller/TestController.java
@@ -3,16 +3,15 @@ package sesuda.controller;
 import jakarta.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sesuda.dto.MemberDTO;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/sesuda")
-public class JoinController {
+public class TestController {
 
     public JSONObject json;
 
@@ -26,5 +25,13 @@ public class JoinController {
         response.setHeader("Access-Control-Allow-OriginAccess-Control-Allow-Origin", "*");
 
         return status;
+    }
+
+    @PostMapping(value = "/test1")
+    public String test1(@RequestBody MemberDTO dto, HttpServletResponse response) {
+
+        System.out.println("dto = " + dto);
+
+        return "success!";
     }
 }
