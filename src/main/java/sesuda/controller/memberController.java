098710051b3088f07bf17765bbdf090b7d65543f
seyoun <seyounham@gmail.com>
2023-03-28 23:55:45 +0900

    변경사항 커밋 -세윤

--- src/main/java/sesuda/controller/memberController.java
+++ src/main/java/sesuda/controller/memberController.java
@@ -1,6 +1,8 @@
 package sesuda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
