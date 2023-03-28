4177f8afcfc6cb797f15aaca426d6b0db649b427
seyoun <seyounham@gmail.com>
2023-03-29 00:57:16 +0900

    MenuController추가

---
+++ src/main/java/sesuda/controller/MenuController.java
@@ -0,0 +1,11 @@
+package sesuda.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+
+}
