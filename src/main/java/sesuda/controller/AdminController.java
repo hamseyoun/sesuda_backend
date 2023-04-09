f90a283cb8e89afedb64bac31bb99c977a545708
seyoun <seyounham@gmail.com>
2023-04-09 19:40:34 +0900

    orderList 주석 추가

--- src/main/java/sesuda/controller/AdminController.java
+++ src/main/java/sesuda/controller/AdminController.java
@@ -39,18 +39,26 @@ public class AdminController {
         MemberDTO dto = adminService.adminCheck(intMemberUid);
 
         String result ="";
-        List<AdminDTO> dtos = new ArrayList<>();
+        List<AdminDTO> dtos;
+
+        //orderUid만 중복 없이 담을 list를 만들어야 하니까 set을 사용하자
         Set<Integer> orderUidsSet = new LinkedHashSet<>();
+
+        //set은 .get(index)로 가져올 수 없으니, iterator로 set에 담겨있는것을 꺼내 list에 다시 담아줘야한다.
         List<Integer> orderUidsList = new ArrayList<>();
+
+        //최종적으로 나온 값들을 담아줄 list
         List<Map<String, Object>> finalList = new ArrayList<>();
 
         if(dto.getAuth().equals("admin")) {
             dtos = adminService.orderList();
 
+            //dtos에서 orderUid만 가져와 set에 넣어주는 과정
             for (int i = 0; i < dtos.size(); i++) {
                 orderUidsSet.add(dtos.get(i).getOrderUid());
             }
 
+            //set에서 hasNext(), next() 이용해 값을 꺼내서 다시 list에 넣어주는 과정
             Iterator<Integer> orderUidsIter = orderUidsSet.iterator();
             while (orderUidsIter.hasNext()) {
                 orderUidsList.add(orderUidsIter.next());
@@ -59,12 +67,16 @@ public class AdminController {
             System.out.println("orderUidsList = " + orderUidsList);
 
 
+            //list의 길이만큼 for문을 돌림 (list의 상태는 [2,3] 이런 형태)
             for (int i = 0; i < orderUidsList.size(); i++) {
                 List<AdminDTO> dtosList = new ArrayList<>();
+
+                //service-dao를 통해 가져온 dtos 사이즈만큼 for문을 돌리는데,
                 for (int j = 0; j < dtos.size(); j++) {
                     int orderUid_i = orderUidsList.get(i);
                     int orderUid_j = dtos.get(j).getOrderUid();
 
+                    //dtos의 orderUid가 set->list의 orderUid 와 일치하는 것만 dtosList에 담아줌
                     if( orderUid_i == orderUid_j ){
                         dtosList.add(dtos.get(j));
                     }
